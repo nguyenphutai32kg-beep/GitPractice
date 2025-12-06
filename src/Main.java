722a42429a7707fbd1807f0b375e608bd1efb3c4
nguyenphutai32kg-beep <nguyenphutai32kg@gmail.com>
2025-12-06 19:19:34 +0700

    Them comment giai thich cho cac buoc khoi tao

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ import assessmentmodule.Assessment;
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== E-LEARNING SYSTEM FULL FLOW ===");
-
+// Khoi tao doi tuong Student
         user student = new user();
         student.id = "SV001";
         student.name = "Nguyễn Phú Tài";
         System.out.println("Dang xu ly du lieu cho sinh vien: " + student.name);
         student.role = "Student";
         student.login();
-
+// Khoi tao khoa hoc Java
         course javaCourse = new course();
         javaCourse.courseId = "Java";
         javaCourse.title = "Java for beginner";
